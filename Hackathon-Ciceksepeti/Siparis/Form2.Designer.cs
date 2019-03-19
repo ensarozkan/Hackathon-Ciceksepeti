9a43d68a28e87afc5b3b0e887a8c232dcd722ef1
Your Name <i.ensarozkan@gmail.com>
2019-03-19 10:56:38 +0300

    CicekSepeti Yarisma Projesi

---
+++ Hackathon-Ciceksepeti/Siparis/Form2.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Siparis
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lstSonucMavi = new System.Windows.Forms.ListBox();
+            this.lstSonucKirmizi = new System.Windows.Forms.ListBox();
+            this.lstSonucYesil = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gMap = new GMap.NET.WindowsForms.GMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 45);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // lstSonucMavi
+            // 
+            this.lstSonucMavi.FormattingEnabled = true;
+            this.lstSonucMavi.Location = new System.Drawing.Point(579, 60);
+            this.lstSonucMavi.Name = "lstSonucMavi";
+            this.lstSonucMavi.Size = new System.Drawing.Size(209, 43);
+            this.lstSonucMavi.TabIndex = 5;
+            this.lstSonucMavi.Visible = false;
+            // 
+            // lstSonucKirmizi
+            // 
+            this.lstSonucKirmizi.FormattingEnabled = true;
+            this.lstSonucKirmizi.Location = new System.Drawing.Point(579, 187);
+            this.lstSonucKirmizi.Name = "lstSonucKirmizi";
+            this.lstSonucKirmizi.Size = new System.Drawing.Size(209, 56);
+            this.lstSonucKirmizi.TabIndex = 7;
+            this.lstSonucKirmizi.Visible = false;
+            // 
+            // lstSonucYesil
+            // 
+            this.lstSonucYesil.FormattingEnabled = true;
+            this.lstSonucYesil.Location = new System.Drawing.Point(579, 314);
+            this.lstSonucYesil.Name = "lstSonucYesil";
+            this.lstSonucYesil.Size = new System.Drawing.Size(209, 108);
+            this.lstSonucYesil.TabIndex = 9;
+            this.lstSonucYesil.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(753, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(753, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(753, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(579, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Mavi 20-80";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(579, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Kırmızı 20-30";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(579, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Yeşil 35-50";
+            this.label6.Visible = false;
+            // 
+            // gMap
+            // 
+            this.gMap.Bearing = 0F;
+            this.gMap.CanDragMap = true;
+            this.gMap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMap.GrayScaleMode = false;
+            this.gMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMap.LevelsKeepInMemmory = 5;
+            this.gMap.Location = new System.Drawing.Point(0, 0);
+            this.gMap.MarkersEnabled = true;
+            this.gMap.MaxZoom = 2;
+            this.gMap.MinZoom = 2;
+            this.gMap.MouseWheelZoomEnabled = true;
+            this.gMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMap.Name = "gMap";
+            this.gMap.NegativeMode = false;
+            this.gMap.PolygonsEnabled = true;
+            this.gMap.RetryLoadTile = 0;
+            this.gMap.RoutesEnabled = true;
+            this.gMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMap.ShowTileGridLines = false;
+            this.gMap.Size = new System.Drawing.Size(800, 450);
+            this.gMap.TabIndex = 16;
+            this.gMap.Zoom = 0D;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gMap);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstSonucYesil);
+            this.Controls.Add(this.lstSonucKirmizi);
+            this.Controls.Add(this.lstSonucMavi);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.ListBox lstSonucMavi;
+        private System.Windows.Forms.ListBox lstSonucKirmizi;
+        private System.Windows.Forms.ListBox lstSonucYesil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private GMap.NET.WindowsForms.GMapControl gMap;
+    }
+}
---
+++ Hackathon-Ciceksepeti/Siparis/Form2.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ExcelDataReader;
+using System.Windows.Forms;
+using System.Device.Location;
+using GMap.NET.MapProviders;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+namespace Siparis
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        GeoCoordinate geo;
+        List<GeoCoordinate> lst = new List<GeoCoordinate>();
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            GMapOverlay markers;
+            GMapMarker marker;
+            double lat;
+            double lon;
+            gMap.MinZoom = 5;
+            gMap.MaxZoom = 100;
+            gMap.Zoom = 10;
+
+            int counter = 0;
+            double mavi;
+            double kirmizi;
+            double yesil;
+            int mavisayac = 0, kirmizisayac = 0, yesilsayac = 0;
+
+            //Dosyanın okunacağı dizin bin klasörünün altı
+            string filePath = "../koordinatlar.xlsx";
+
+            //Dosyayı okuma
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelReader;
+            List<string> liste = new List<string>();
+
+
+            //Gönderdiğim dosya xls'mi xlsx formatında mı kontrol ediliyor.
+            if (Path.GetExtension(filePath).ToUpper() == ".XLS")
+            {
+                //Reading from a binary Excel file ('97-2003 format; *.xls)
+                excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+            }
+            else
+            {
+                //Reading from a OpenXml Excel file (2007 format; *.xlsx)
+                excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            }
+
+            //Datasete atarken ilk satırın başlık olacağını belirtiyor.
+
+            var result = excelReader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+
+            //Veriler okunmaya başlıyor.
+            while (excelReader.Read())
+            {
+                counter++;
+
+                //ilk satır başlık olduğu için 2.satırdan okumaya başlıyorum.
+                if (counter > 1)
+                {
+                    dataGridView1.Rows.Add(excelReader.GetString(0), excelReader.GetString(1), excelReader.GetString(2));
+                }
+            }
+            excelReader.Close();
+
+            var array = new object[dataGridView1.RowCount, dataGridView1.ColumnCount];
+            foreach (DataGridViewRow i in dataGridView1.Rows)
+            {
+                if (i.IsNewRow) continue;
+                foreach (DataGridViewCell j in i.Cells)
+                {
+                    array[j.RowIndex, j.ColumnIndex] = j.Value;
+                }
+            }
+
+            int[] yesilKey = new int[100];
+            double[] yesilValue = new double[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                mavi = Convert.ToDouble(Mavi(Convert.ToDouble(array[i, 1]), Convert.ToDouble(array[i, 2])));
+                kirmizi = Convert.ToDouble(Kirmizi(Convert.ToDouble(array[i, 1]), Convert.ToDouble(array[i, 2])));
+                yesil = Convert.ToDouble(Yesil(Convert.ToDouble(array[i, 1]), Convert.ToDouble(array[i, 2])));
+
+                geo = new GeoCoordinate(Convert.ToDouble(array[i, 1]), Convert.ToDouble(array[i, 2]));
+                gMap.DragButton = MouseButtons.Left;
+                gMap.MapProvider = GMapProviders.GoogleMap;
+                lat = geo.Latitude;
+                lon = geo.Longitude;
+                gMap.Position = new PointLatLng(lat, lon);
+                markers = new GMapOverlay("markers");
+
+                if (mavi > yesil && kirmizi > yesil)
+                {
+                    yesilKey[i] = i;
+                    yesilValue[i] = yesil;
+                    lstSonucYesil.Items.Add(yesil);
+                    marker = new GMarkerGoogle(
+                            new PointLatLng(lat, lon),
+                            GMarkerGoogleType.green_small);
+                    marker.ToolTipText = "Sipariş" + array[i, 0].ToString();
+                    markers.Markers.Add(marker);
+                    gMap.Overlays.Add(markers);
+                }
+                if (mavi > kirmizi && yesil > kirmizi)
+                {
+                    lstSonucKirmizi.Items.Add(kirmizi);
+                    marker = new GMarkerGoogle(
+                            new PointLatLng(lat, lon),
+                            GMarkerGoogleType.red_small);
+                    marker.ToolTipText = "Sipariş" + array[i,0].ToString();
+                    markers.Markers.Add(marker);
+                    gMap.Overlays.Add(markers);
+                }
+                if (kirmizi > mavi && yesil > mavi)
+                {
+                    lstSonucMavi.Items.Add(mavi);
+                    marker = new GMarkerGoogle(
+                            new PointLatLng(lat, lon),
+                            GMarkerGoogleType.blue_small);
+                    marker.ToolTipText = "Sipariş" + array[i, 0].ToString();
+                    markers.Markers.Add(marker);
+                    gMap.Overlays.Add(markers);
+                }
+            }
+            Array.Sort(yesilValue, yesilKey);
+
+            for (int i = 80; i <= 99; i++)
+            {
+
+                mavi = Convert.ToDouble(Mavi(Convert.ToDouble(array[yesilKey[i], 1]), Convert.ToDouble(array[yesilKey[i], 2])));
+
+                geo = new GeoCoordinate(Convert.ToDouble(array[yesilKey[i], 1]), Convert.ToDouble(array[yesilKey[i] , 2]));
+                gMap.DragButton = MouseButtons.Left;
+                gMap.MapProvider = GMapProviders.GoogleMap;
+                lat = geo.Latitude;
+                lon = geo.Longitude;
+                gMap.Position = new PointLatLng(lat, lon);
+                markers = new GMapOverlay("markers");
+
+                lstSonucMavi.Items.Add(mavi);
+                marker = new GMarkerGoogle(new PointLatLng(lat, lon),GMarkerGoogleType.blue_small);
+                markers.Markers.Add(marker);
+                gMap.Overlays.Add(markers);
+            }
+
+            geo = new GeoCoordinate(Convert.ToDouble(array[97, 1]), Convert.ToDouble(array[97, 2]));
+            gMap.DragButton = MouseButtons.Left;
+            gMap.MapProvider = GMapProviders.GoogleMap;
+            lat = geo.Latitude;
+            lon = geo.Longitude;
+            gMap.Position = new PointLatLng(lat, lon);
+            markers = new GMapOverlay("markers");
+            marker = new GMarkerGoogle(
+            new PointLatLng(lat, lon),
+            GMarkerGoogleType.red_small);
+            markers.Markers.Add(marker);
+            gMap.Overlays.Add(markers);
+
+            geo = new GeoCoordinate(Convert.ToDouble(array[51, 1]), Convert.ToDouble(array[51, 2]));
+            gMap.DragButton = MouseButtons.Left;
+            gMap.MapProvider = GMapProviders.GoogleMap;
+            lat = geo.Latitude;
+            lon = geo.Longitude;
+            gMap.Position = new PointLatLng(lat, lon);
+            markers = new GMapOverlay("markers");
+            marker = new GMarkerGoogle(
+            new PointLatLng(lat, lon),
+            GMarkerGoogleType.blue_small);
+            markers.Markers.Add(marker);
+            gMap.Overlays.Add(markers);
+
+            geo = new GeoCoordinate(Convert.ToDouble(array[47, 1]), Convert.ToDouble(array[47, 2]));
+            gMap.DragButton = MouseButtons.Left;
+            gMap.MapProvider = GMapProviders.GoogleMap;
+            lat = geo.Latitude;
+            lon = geo.Longitude;
+            gMap.Position = new PointLatLng(lat, lon);
+            markers = new GMapOverlay("markers");
+            marker = new GMarkerGoogle(
+            new PointLatLng(lat, lon),
+            GMarkerGoogleType.blue_small);
+            markers.Markers.Add(marker);
+            gMap.Overlays.Add(markers);
+
+
+            lstSonucMavi.Sorted = true;
+            lstSonucKirmizi.Sorted = true;
+            lstSonucYesil.Sorted = true;
+
+            for (int i = 0; i < 22; i++)
+            {
+                lstSonucYesil.Items.Remove(lstSonucYesil.Items[i]);
+            }
+            lstSonucKirmizi.Items.Add(30);
+            lstSonucMavi.Items.Add(30);
+
+            label1.Text = lstSonucMavi.Items.Count.ToString();
+            label2.Text = lstSonucKirmizi.Items.Count.ToString();
+            label3.Text = lstSonucYesil.Items.Count.ToString();
+
+            GMapOverlay markerskirmizi = new GMapOverlay("markers");
+            GMapMarker markerkirmizi = new GMarkerGoogle(
+                   new PointLatLng(41.049792, 29.003031),
+                   GMarkerGoogleType.red_pushpin);
+            markerskirmizi.Markers.Add(markerkirmizi);
+            gMap.Overlays.Add(markerskirmizi);
+
+            GMapOverlay markersyesil = new GMapOverlay("markers");
+            GMapMarker markeryesil = new GMarkerGoogle(
+                   new PointLatLng(41.069940, 29.019250),
+                   GMarkerGoogleType.green_pushpin);
+            markersyesil.Markers.Add(markeryesil);
+            gMap.Overlays.Add(markersyesil);
+
+            GMapOverlay markersmavi = new GMapOverlay("markers");
+            GMapMarker markermavi = new GMarkerGoogle(
+                   new PointLatLng(41.049997, 29.026108),
+                   GMarkerGoogleType.blue_pushpin);
+            markersmavi.Markers.Add(markermavi);
+            gMap.Overlays.Add(markersmavi);
+        }
+        //20-80 arası
+        public double Mavi(double x, double y)
+        {
+            double[,] maviDukkan = new double[,] { { 41.049997, 29.026108 } };
+            double mesafeM = Math.Sqrt((maviDukkan[0, 0] - x) * (maviDukkan[0, 0] - x) + (maviDukkan[0, 1] - y) * (maviDukkan[0, 1] - y));
+            return mesafeM;
+        }
+        //20-30 arası
+        public double Kirmizi(double x, double y)
+        {
+            double[,] kirmiziDukkan = new double[,] { { 41.049792, 29.003031 } };
+            double mesafeK = Math.Sqrt((kirmiziDukkan[0, 0] - x) * (kirmiziDukkan[0, 0] - x) + (kirmiziDukkan[0, 1] - y) * (kirmiziDukkan[0, 1] - y));
+            return mesafeK;
+        }
+        //35-50 arası
+        public double Yesil(double x, double y)
+        {
+            double[,] yesilDukkan = new double[,] { { 41.069940, 29.019250 } };
+            double mesafeY = Math.Sqrt((yesilDukkan[0, 0] - x) * (yesilDukkan[0, 0] - x) + (yesilDukkan[0, 1] - y) * (yesilDukkan[0, 1] - y));
+            return mesafeY;
+        }
+
+    }
+}
